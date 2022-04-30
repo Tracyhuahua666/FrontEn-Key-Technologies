aaca9c266ca7881c0e6d8b3f04f48a70a556265f
Tracyhuahua <13145916372@163.com>
2022-04-30 16:41:52 +0800

    js 深复制与浅复制

---
+++ JavaScript经典手写/浅复制与深复制.js
@@ -0,0 +1,56 @@
+//浅拷贝方法1：
+function simpleCopy(target) {
+    let cloneTarget = {}
+    for(i in target){
+        cloneTarget[i]=target[i]
+    }
+    return cloneTarget
+}
+// 浅拷贝方法2
+objClone = {...obj} //ES6的对象解构，这个也是浅复制
+/**
+ * 深拷贝
+ * @param {Object} obj 要拷贝的对象
+ * @param {Map} map 用于存储循环引用对象的地址
+ */
+ function deepCopy(obj, map = new Map()) {
+    if (typeof obj !== "object" || obj === null) {
+      return obj;
+    }
+    if (map.get(obj)) {
+      return map.get(obj);
+    }
+    // 初始化返回结果
+    let result = Array.isArray(obj)?[]:{}
+    // 防止循环引用
+    map.set(obj, result);
+    for (const key in obj) {
+        // 递归调用
+        result[key] = deepCopy(obj[key], map);
+    }
+    // 返回结果
+    return result;
+  }
+  
+let obj = {
+    a:undefined,
+    b:{
+        c: function() {
+            console.log(1)
+        },
+        d: null,
+        g: NaN,
+        i:'hello',
+        j:true,
+        k: undefined
+    },
+    h:[1,2,3,4]
+}
+
+obj.a=obj//添加循环引用
+
+//测试代码
+let obj_copy = deepCopy(obj)
+obj_copy.h.push(5)
+console.log('obj',obj)
+console.log('obj_copy',obj_copy)
