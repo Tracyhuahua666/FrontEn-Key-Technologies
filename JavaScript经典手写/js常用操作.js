ed6f30496970aa37bc103d497a77d8f5b428648f
Tracyhuahua <13145916372@163.com>
2022-06-11 15:01:01 +0800

    ll

--- JavaScript经典手写/js常用操作.js
+++ JavaScript经典手写/js常用操作.js
@@ -9,8 +9,16 @@ console.log('str1:',[...str1])
 arr2.push('d') // ['a','b','c','d']
 arr2.pop() // ['a','b','c']
 
-//操作数组结尾
-arr1.push() 
+//操作数组头操作
+// unshift在列表头部插入元素，返回插入后数组长度
+let arr2_unshift_return = arr2.unshift('d')
+console.log('arr2_unshift_return:',arr2_unshift_return)
+console.log('arr2:',arr2)
+
+// shift 去掉头结点
+let arr2_shift_return = arr2.shift()
+console.log('arr2_shift_return:',arr2_shift_return)
+console.log('arr2:',arr2)
 
 // 逆转数组
 arr3 = arr2.reverse()
